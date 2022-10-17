8b7db6deb39c014a2d2adc6f62185f4364da16b0
baraclese <1628879+baraclese@users.noreply.github.com>
2022-10-17 19:58:03 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Zombie Lag Fix
+
+A Minecraft 1.6.4 legacy fabric mod that reduces lag caused by zombie pathfinding.
+To do that it uses the pathfinding throttling code from 1.9.4.
---
+++ src/main/java/baraclese/zombie_lag_fix/ZombieLagFixMod.java
@@ -0,0 +1,10 @@
+package baraclese.zombie_lag_fix;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ZombieLagFixMod implements ModInitializer
+{
+	@Override
+	public void onInitialize()
+	{}
+}
